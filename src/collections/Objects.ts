3ef185cbbc54e47efcb7c1c14ce56d93de2fd628
Garcez <allan.f.garcez@gmail.com>
2018-12-08 04:17:08 -0200

    Deep search key in object

--- src/collections/Objects.ts
+++ src/collections/Objects.ts
@@ -1,7 +1,12 @@
-export const keyAsValue = (obj: any) =>
-  Object.keys(obj).reduce((acc: any, key) => {
-    const val = obj[key];
-    acc[val] = acc[val] || {};
-    acc[val] = key;
-    return acc;
-  }, {});
+const findByKey = (prop: string, object: any): any => {
+  for (let x of Object.keys(object)) {
+    if (prop === x) {
+      return {
+        [x]: object[x],
+      };
+    }
+    if (typeof object[x] === 'object' || Array.isArray(object[x])) {
+      return findByKey(prop, object[x]);
+    }
+  }
+};

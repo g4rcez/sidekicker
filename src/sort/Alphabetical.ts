1b61ec319374c48576621e5dabb6c6d418360f57
Garcez <allan.f.garcez@gmail.com>
2018-12-08 04:19:26 -0200

    Chars sort

--- src/sort/Alphabetical.ts
+++ src/sort/Alphabetical.ts
@@ -1,19 +1,17 @@
 import Strings from '../comparable/Strings';
 
-const sort = (sensitive: boolean = false, array: string[]) => {
-  if (sensitive) {
-    return array.sort((a, b) => {
-      if (Strings.equals(a, b)) {
-        return 0;
-      }
-      return Strings.firstAlphabeticalOrderSensitive(a, b) ? -1 : 1;
-    });
+const insensitive = (a: string, b: string) => {
+  if (Strings.equals(a, b)) {
+    return 0;
   }
-  return array.sort((a, b) => {
-    if (Strings.equalsCaseInsensitive(a, b)) {
-      return 0;
-    }
-    return Strings.firstAlphabeticalOrderInsensitive(a, b) ? -1 : 1;
-  });
+  return Strings.firstAlphabeticalOrderInsensitive(a, b) ? -1 : 1;
 };
-export default sort;
+
+const sensitive = (a: string, b: string) => {
+  if (Strings.equals(a, b)) {
+    return 0;
+  }
+  return Strings.firstAlphabeticalOrderSensitive(a, b) ? -1 : 1;
+};
+
+export default { sensitive, insensitive };

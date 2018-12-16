e1a1858ebce3de50fd1738bc6b8fa08641a36418
Garcez <allan.f.garcez@gmail.com>
2018-12-16 18:54:56 -0200

    Correctly import

--- src/sort/Alphabetical.ts
+++ src/sort/Alphabetical.ts
@@ -1,17 +1,17 @@
-import Strings from '../comparable/Strings';
+import Strings from "../comparable/Strings";
 
 const insensitive = (a: string, b: string) => {
-  if (Strings.equals(a, b)) {
-    return 0;
-  }
-  return Strings.firstAlphabeticalOrderInsensitive(a, b) ? -1 : 1;
+    if (Strings.equals(a, b)) {
+        return 0;
+    }
+    return Strings.firstAlphabeticalOrderInsensitive(a, b) ? -1 : 1;
 };
 
 const sensitive = (a: string, b: string) => {
-  if (Strings.equals(a, b)) {
-    return 0;
-  }
-  return Strings.firstAlphabeticalOrderSensitive(a, b) ? -1 : 1;
+    if (Strings.equals(a, b)) {
+        return 0;
+    }
+    return Strings.firstAlphabeticalOrderSensitive(a, b) ? -1 : 1;
 };
 
 export default { sensitive, insensitive };

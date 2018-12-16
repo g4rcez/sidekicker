9ae4105031839a96fa542cb96f5544ed933d579e
Garcez <allan.f.garcez@gmail.com>
2018-12-16 18:53:35 -0200

    Correction on rightPipe

--- src/functional/Pipe.ts
+++ src/functional/Pipe.ts
@@ -1,5 +1,5 @@
-export const rightPipe = (...fns: Function[]) => fns.reverse().reduce((f, g) => (...args: any) => f(g(...args)));
+export const rightPipe = (...fns: Function[]) => fns.reduce((f, g) => (...args: any) => g(f(...args)));
 
-export const leftPipe = (...fns: Function[]) => fns.reverse().reduce((f, g) => (...args: any) => f(g(...args)));
+export const leftPipe = (...fns: Function[]) => fns.reduce((f, g) => (...args: any) => f(g(...args)));
 
 export default { right: rightPipe, left: leftPipe };

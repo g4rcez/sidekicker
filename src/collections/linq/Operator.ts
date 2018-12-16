53e706868c2e9d465b6d62b6f358feab1adaf7b1
Garcez <allan.f.garcez@gmail.com>
2018-12-16 18:51:14 -0200

    Add more operators

--- src/collections/linq/Operator.ts
+++ src/collections/linq/Operator.ts
@@ -1,13 +1,18 @@
-import Operator from '../../types/Operator';
+import { Operator } from "../../types/Operator";
 
 export const operators: Operator = {
-  like: (x: any, y: any): Boolean => {
-    const str: string = x.toString();
-    return !!str.match(new RegExp(`.*${y.toString()}.*`, 'gi'));
-  },
-  '==': (x: any, y: any): Boolean => x == y,
-  '===': (x: any, y: any): Boolean => x === y,
-  '!=': (x: any, y: any): Boolean => x != y,
+    like: (x: any, y: any): Boolean => {
+        const str: string = x.toString();
+        return !!str.match(new RegExp(`.*${y.toString()}.*`, "gi"));
+    },
+    "==": (x: any, y: any): Boolean => x == y,
+    ">": (x: any, y: any): Boolean => x > y,
+    ">=": (x: any, y: any): Boolean => x >= y,
+    "<": (x: any, y: any): Boolean => x < y,
+    "<=": (x: any, y: any): Boolean => x <= y,
+    "!=": (x: any, y: any): Boolean => x != y,
+    "!==": (x: any, y: any): Boolean => x !== y,
+    "===": (x: any, y: any): Boolean => x === y,
 };
 
 export const executeByOperator = (x: any, operator: string, y: any) => operators[operator](x, y);

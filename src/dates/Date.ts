f11491459c08a5f8792d80dda643161e1a148ab6
Garcez <allan.f.garcez@gmail.com>
2018-12-16 18:52:52 -0200

    Add more moment comparators

--- src/dates/Date.ts
+++ src/dates/Date.ts
@@ -1,9 +1,11 @@
-import moment from 'moment';
+import moment from "moment";
 
-export const afterToday = (dateCompare: string) => moment(dateCompare).isAfter(moment(), 'day');
+export const afterToday = (dateCompare: string) => moment(dateCompare).isAfter(moment(), "day");
 
-export const today = (dateCompare: string) => moment(dateCompare).isSame(moment(), 'day');
+export const beforeToday = (dateCompare: string) => moment(dateCompare).isBefore(moment(), "day");
 
-export const sameMouth = (date: string) => moment(date).isSame(moment(), 'month');
+export const today = (dateCompare: string) => moment(dateCompare).isSame(moment(), "day");
 
-export const sameYear = (date: string) => moment(date).isSame(moment(), 'year');
+export const sameMouth = (date: string) => moment(date).isSame(moment(), "month");
+
+export const sameYear = (date: string) => moment(date).isSame(moment(), "year");

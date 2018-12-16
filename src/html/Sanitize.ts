576052b5cac169fd01322fe5a0a91bed49b9e1cb
Garcez <allan.f.garcez@gmail.com>
2018-12-16 18:53:51 -0200

    Correctly import

--- src/html/Sanitize.ts
+++ src/html/Sanitize.ts
@@ -1,4 +1,4 @@
-import { trueTrim } from './../strings/Transform';
+import { trueTrim } from '../strings/Utils';
 
 export const noHtml = (string: string) => {
   return trueTrim(string.replace(/<[^>]*>/g, ''));

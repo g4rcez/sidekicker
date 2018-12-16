a78d38b61a4ea3870dc1b842a76513d2091594b4
Garcez <allan.f.garcez@gmail.com>
2018-12-16 18:54:23 -0200

    Add more functions to export (need some tests yet)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { default as Validations } from './validations';
+export { default as Validations } from "./validations";
+export { default as Collections } from "./collections";
+export { default as Strings } from "./strings";
+export { default as Regex } from "./regex";

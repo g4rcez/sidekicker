53f3263114dfa6961560642a2c2dd4151b323355
Garcez <allan.f.garcez@gmail.com>
2019-01-06 08:23:39 -0200

    import style

--- src/regex/GenericRegex.ts
+++ src/regex/GenericRegex.ts
@@ -1,2 +1,2 @@
-export const onlyDigit = /^[0-9]+$/g
-export const onlyChars = /[a-záàãéèẽíìĩóòõúùũâêîôû]+/gi
+export const ONLY_DIGIT = /^[0-9]+$/g
+export const ONLY_CHARS = /[a-záàãéèẽíìĩóòõúùũâêîôû]+/gi

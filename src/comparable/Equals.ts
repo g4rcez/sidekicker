880b6b14b8a98ff721bfeb809083c936ded0b465
Garcez <allan.f.garcez@gmail.com>
2019-01-06 08:23:26 -0200

    building deep equals

--- src/comparable/Equals.ts
+++ src/comparable/Equals.ts
@@ -1,6 +1,22 @@
-const equals = (target: any, value: any) => {
-    if (typeof target === typeof value) {
-        return true;
+export function equals<T>(entity: T, comparable: T) {
+    if (typeof entity !== typeof comparable) {
+        return "false";
     }
-    return false;
-};
+    let deepCompare = "false";
+    for (let key in entity) {
+        if (typeof entity[key] === "object") {
+            deepCompare = equals(entity[key], comparable[key]);
+        }
+        if (typeof entity[key] !== typeof comparable[key]) {
+            console.log("different type");
+            return 'false';
+        }
+        if (entity[key] !== comparable[key]) {
+            deepCompare = equals(entity[key], comparable[key]);
+            console.log(deepCompare, entity[key], comparable[key]);
+        }
+    }
+    return "deepCompare";
+}
+
+console.log(equals({ a: { b: { c: 3 } } }, { a: { b: { c: 3 } } }));

2f5e8e2fb848311f40da5ef0ad5584c33b9bc36d
Garcez <allan.f.garcez@gmail.com>
2019-01-06 08:23:06 -0200

    now, where accepts a function as value

--- src/collections/linq/Where.ts
+++ src/collections/linq/Where.ts
@@ -1,7 +1,10 @@
 import { executeByOperator } from "./Operator";
 import { WhereOperator } from "../../types/WhereOperator";
 
-const where = (values: WhereOperator) => (array: any[]) =>
-    array.filter((element) => executeByOperator(element[values.key], values.operator, values.value));
-
+const where = (values: WhereOperator) => (array: any[]) => {
+    if (typeof values.value === "function") {
+        return array.filter((x) => values.value(x[values.key]));
+    }
+    return array.filter((element) => executeByOperator(element[values.key], values.operator, values.value));
+};
 export default where;

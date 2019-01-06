9b682058dbce1edd382c17c693d8511317b34b4b
Garcez <allan.f.garcez@gmail.com>
2019-01-06 08:24:06 -0200

    delete forgotten console.log

--- src/strings/unix/Tr.ts
+++ src/strings/unix/Tr.ts
@@ -7,7 +7,6 @@ interface TR {
 const Tr = (tr: TR) => {
   let text = [...tr.text];
   if (tr.subs1.length === tr.subs2.length) {
-    console.log(tr.subs1, tr.subs2);
     [...tr.subs1].forEach((x, i) => {
       if (text.indexOf(x) !== tr.text.search(x)) {
         text[text.indexOf(x)] = tr.subs2[i];

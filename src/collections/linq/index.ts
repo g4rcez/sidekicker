9e5ec17fee3c92560d69c9138ebc95c85a9e8579
Garcez <allan.f.garcez@gmail.com>
2019-01-06 08:22:37 -0200

    Organize imports and add new functions

--- src/collections/linq/index.ts
+++ src/collections/linq/index.ts
@@ -5,30 +5,56 @@ import { WhereOperator } from "../../types/WhereOperator";
 import { Linqs } from "../../types/Linqs";
 import { OrdersRules } from "../../types/OrderByParameters";
 import OrderBy from "./OrderBy";
+import Map from "./map";
+import concat from "./concat";
 
-const Linq = (array: any[]) => {
+const sum = (key: string, array: any) =>
+    array.reduce((acc: number, el: any) => {
+        return (acc += el[key]);
+    }, 0);
+
+export function Linq<T>(array: T[]) {
     const functions: Function[] = [];
-    const linqs: Linqs = {
-        where: (conditions: WhereOperator): Linqs => {
-            functions.push(Where(conditions));
+    const linqs: Linqs<T> = {
+        average: (key: string) => {
+            const arr = linqs.get();
+            return sum(key, linqs.get()) / arr.length;
+        },
+        get: (): T[] => functions.reduce((acc, curr) => curr(acc), array),
+        groupBy: (key: string) => {
+            functions.push(GroupBy(key));
+            return linqs.get;
+        },
+        head: (): T => linqs.get()[0],
+        join: (toConcat: T[]) => {
+            functions.push(concat(toConcat));
+            return linqs;
+        },
+        map: (fn: Function): Linqs<T> => {
+            functions.push(Map(fn));
+            return linqs;
+        },
+        orderBy: (key: string, rules?: OrdersRules) => {
+            functions.push(OrderBy(key, rules));
             return linqs;
         },
         paginate: (range: number, page: number = 0) => {
             functions.push(Paginate(range, page));
             return linqs;
         },
-        groupBy: (key: string) => {
-            functions.push(GroupBy(key));
-            return linqs.execute;
-        },
-        orderBy: (key: string, rules?: OrdersRules) => {
-            functions.push(OrderBy(key, rules));
+        reverse: () => {
+            functions.push((array: T[]) => array.reverse());
             return linqs;
         },
-        execute: (): any[] => functions.reduce((acc, curr) => curr(acc), array),
-        run: () => linqs.execute(),
+        select: (): T[] => linqs.get(),
+        sum: (key: string) => {
+            return sum(key, linqs.get());
+        },
+        tail: (): T => linqs.get().slice(-1)[0],
+        where: (conditions: WhereOperator): Linqs<T> => {
+            functions.push(Where(conditions));
+            return linqs;
+        },
     };
     return Object.freeze(linqs);
-};
-
-export default Linq;
+}

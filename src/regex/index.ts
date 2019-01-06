9bfb62a01e7f12d109fc7283d6d317eddc44c476
Garcez <allan.f.garcez@gmail.com>
2019-01-06 08:23:45 -0200

    import style

--- src/regex/index.ts
+++ src/regex/index.ts
@@ -1,16 +1,3 @@
-import { CEP, CNPJ, CPF, plate } from "./BrazilianRegex";
-import { onlyChars, onlyDigit } from "./GenericRegex";
-import { username, email, http, hexColor } from "./WebRegex";
-
-export default {
-    CEP,
-    CNPJ,
-    CPF,
-    plate,
-    onlyChars,
-    onlyDigit,
-    username,
-    email,
-    http,
-    hexColor,
-};
+export * from "./BrazilianRegex";
+export * from "./GenericRegex";
+export * from "./WebRegex";

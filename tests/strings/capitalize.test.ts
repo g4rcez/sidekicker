e5b2c4f6acc1248f6074b024cb72a6771f78ecf8
Garcez <allan.f.garcez@gmail.com>
2019-01-08 13:22:09 -0200

    Correct tslint and add tests

--- tests/strings/capitalize.test.ts
+++ tests/strings/capitalize.test.ts
@@ -1,12 +1,12 @@
-import { capitalize } from '../../src/strings';
+import { capitalize } from "../../src/strings";
 
 const tests = [
-  { toBe: 'Everything ok', expect: capitalize('eVERyTHing OK'), name: 'multiple cases' },
-  { toBe: 'Marvel vs dc', expect: capitalize('MARVEL VS DC'), name: 'versus mode' },
+    { toBe: "Everything ok", expect: capitalize("eVERyTHing OK"), name: "multiple cases" },
+    { toBe: "Marvel vs dc", expect: capitalize("MARVEL VS DC"), name: "versus mode" },
 ];
 
 tests.forEach((testing) => {
-  test(testing.name, () => {
-    expect(testing.expect).toBe(testing.toBe);
-  });
+    test(testing.name, () => {
+        expect(testing.expect).toBe(testing.toBe);
+    });
 });

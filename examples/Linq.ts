63f36c5fb9fc0b3f7949d0f605ff79a2d8dc159a
Garcez <allan.f.garcez@gmail.com>
2019-01-08 13:46:21 -0200

    Linq example

--- examples/Linq.ts
+++ examples/Linq.ts
@@ -3,18 +3,19 @@ import { Linq } from "../src/";
 const list = Linq([
     { foo: "bar", id: 1, value: 500 },
     { foo: "bar", id: 2, value: 1050 },
+    { foo: "bar", id: 2, value: 1050 },
+    { foo: "bar", id: 2, value: 1050 },
+    { foo: "bar", id: 2, value: 1050 },
     { foo: "bar", id: 3, value: 750 },
     { foo: "bar", id: 4, value: 501 },
+    { foo: "bar", id: 4, value: 502 },
 ])
-    .where({
-        key: "id",
-        value: (x: any) => x % 2 === 0,
-    })
     .where({
         key: "value",
         operator: ">",
         value: 10,
-    });
-
-console.log(list.average("value"));
-console.log(list.join([{ foo: "bar", id: 5, value: 700 }]).head())
+    })
+    .uniqBy("id")
+    .reverse()
+    .orderBy("value");
+console.log(list.select());

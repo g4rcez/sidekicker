a2734ce12cccffdab48e1353f18c2ba996973d92
Garcez <allan.f.garcez@gmail.com>
2019-01-21 02:40:39 -0200

    delete math folder functions

--- src/math/Factorial.ts
+++
@@ -1,8 +0,0 @@
-const Factorial = (target: number) => {
-    return Array(target)
-        .fill(1)
-        .map((x, i) => i + 1)
-        .reduce((acc, el) => acc * el);
-};
-
-export default Factorial;

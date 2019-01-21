5010851bf81b3dd6b5b2d1f0159f7350c8519a3c
Garcez <allan.f.garcez@gmail.com>
2019-01-21 02:02:42 -0200

    Build some regex

--- src/regex/BrazilianRegex.ts
+++ src/regex/BrazilianRegex.ts
@@ -1,7 +1,7 @@
-export const CEP = /^([\d]{2})\.?([\d]{3})-?([\d]{3})/;
+export const REGEX_CEP = /^([\d]{2})\.?([\d]{3})-?([\d]{3})/;
 
-export const CPF = /^(\d{3}\.){2}\d{3}-\d{2}/;
+export const REGEX_CPF = /^(\d{3}\.){2}\d{3}-\d{2}/;
 
-export const CNPJ = /^\d{2}((|\.)\d{3}){2}(|\/)\d{4}(|-)\d\d$/;
+export const REGEX_CNPJ = /^\d{2}((|\.)\d{3}){2}(|\/)\d{4}(|-)\d\d$/;
 
-export const plate = /^[A-Z]{3}(-| |)\d{4}/i;
+export const REGEX_PLATE = /^[A-Z]{3}(-| |)\d{4}/i;
--- src/regex/GenericRegex.ts
+++ src/regex/GenericRegex.ts
@@ -1,2 +1,2 @@
-export const ONLY_DIGIT = /^[0-9]+$/g
-export const ONLY_CHARS = /[a-záàãéèẽíìĩóòõúùũâêîôû]+/gi
+export const ONLY_DIGIT = /^[0-9]+$/g;
+export const ONLY_CHARS = /[a-záàãéèẽíìĩóòõúùũâêîôû]+/gi;

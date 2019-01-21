8b30e18a4ae89c66cb7b5ea3e88129fd8a59b0b5
Garcez <allan.f.garcez@gmail.com>
2019-01-21 02:02:16 -0200

    Deep equals for all types

--- src/comparable/Equals.ts
+++ src/comparable/Equals.ts
@@ -1,22 +1,64 @@
-export function equals<T>(entity: T, comparable: T) {
-    if (typeof entity !== typeof comparable) {
-        return "false";
-    }
-    let deepCompare = "false";
-    for (let key in entity) {
-        if (typeof entity[key] === "object") {
-            deepCompare = equals(entity[key], comparable[key]);
-        }
-        if (typeof entity[key] !== typeof comparable[key]) {
-            console.log("different type");
-            return 'false';
-        }
-        if (entity[key] !== comparable[key]) {
-            deepCompare = equals(entity[key], comparable[key]);
-            console.log(deepCompare, entity[key], comparable[key]);
-        }
-    }
-    return "deepCompare";
-}
+const props = Object.prototype.hasOwnProperty;
 
-console.log(equals({ a: { b: { c: 3 } } }, { a: { b: { c: 3 } } }));
+const isObject = (a: any, b: any) => a && b && typeof a === "object" && typeof b === "object";
+
+const createInstanceOf = (a: any, b: any, instance: any) => {
+    return [a instanceof instance, b instanceof instance];
+};
+
+export function deepEquals(value: any, target: any) {
+    if (value === target) {
+        return true;
+    }
+    if (!isObject(value, target)) {
+        return value !== value && target !== target;
+    }
+    const targetArr = Array.isArray(value);
+    const valueArr = Array.isArray(target);
+    let length;
+    if (targetArr && valueArr) {
+        length = value.length;
+        if (length !== target.length) {
+            return false;
+        }
+        for (let i = length; i-- !== 0; ) {
+            if (!deepEquals(value[i], target[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    if (targetArr !== valueArr) {
+        return false;
+    }
+    const [valueDate, targetDate] = createInstanceOf(value, target, Date);
+    if (valueDate !== targetDate) {
+        return false;
+    }
+    if (valueDate && targetDate) {
+        return value.getTime() === target.getTime();
+    }
+    const [regexValue, regexTarget] = createInstanceOf(value, target, RegExp);
+    if (regexValue !== regexTarget) {
+        return false;
+    }
+    if (regexValue && regexTarget) {
+        return value.toString() === target.toString();
+    }
+    const objectKeys = Object.keys(value);
+    length = objectKeys.length;
+    if (length !== Object.keys(target).length) {
+        return false;
+    }
+    for (let i = length; i-- !== 0; ) {
+        if (!props.call(target, objectKeys[i])) {
+            return false;
+        }
+    }
+    for (let i = length; i-- !== 0; ) {
+        if (!deepEquals(value[objectKeys[i]], target[objectKeys[i]])) {
+            return false;
+        }
+    }
+    return true;
+}

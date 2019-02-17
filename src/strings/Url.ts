cba6ed35e9fa0f05c31957cc92a95cb9c5020cae
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:43:02 -0200

    Prettier URLs

--- src/strings/Url.ts
+++ src/strings/Url.ts
@@ -1,40 +1,40 @@
 export function urlOnlyParameters(urlString: string) {
-    return urlString.match(/[^&?]*?=[^&?]*/g);
+	return urlString.match(/[^&?]*?=[^&?]*/g);
 }
 
 export function splitUrlValues(param: string): string[] {
-    return param.split("=");
+	return param.split("=");
 }
 
 export function getUrlParameters(urlString: string) {
-    return urlString.split("&");
+	return urlString.split("&");
 }
 
 export function parameterKeyAndValue(parameter: string): Array<string[]> {
-    return getUrlParameters(parameter).map(splitUrlValues);
+	return getUrlParameters(parameter).map(splitUrlValues);
 }
 
 export function urlParameters(urlString: string) {
-    return urlOnlyParameters(urlString)
-        .map((parameter) => new Set(parameterKeyAndValue(parameter)))
-        .reduce((acc, el) => {
-            const [name, value] = el.values().next().value;
-            try {
-                return { ...acc, [name]: JSON.parse(value) };
-            } catch (error) {
-                return { ...acc, [name]: value };
-            }
-        }, {});
+	return urlOnlyParameters(urlString)
+		.map((parameter) => new Set(parameterKeyAndValue(parameter)))
+		.reduce((acc, el) => {
+			const [name, value] = el.values().next().value;
+			try {
+				return { ...acc, [name]: JSON.parse(value) };
+			} catch (error) {
+				return { ...acc, [name]: value };
+			}
+		}, {});
 }
 
 export function urlProtocol(urlString: string = "") {
-    return urlString.split("://")[0];
+	return urlString.split("://")[0];
 }
 
 export const url = {
-    getUrlParameters,
-    splitUrlValues,
-    urlOnlyParameters,
-    urlParameters,
-    urlProtocol,
+	getUrlParameters,
+	splitUrlValues,
+	urlOnlyParameters,
+	urlParameters,
+	urlProtocol,
 };

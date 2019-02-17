ae564bd08968c162c6870245e71e70cd022ec5e1
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:32:37 -0200

    prettier strings

--- src/comparable/Strings.ts
+++ src/comparable/Strings.ts
@@ -1,30 +1,30 @@
 export function strEquals(string: string, compare: string) {
-    return string === compare;
+	return string === compare;
 }
 
 export function equalsCaseInsensitive(string: string, compare: string) {
-    return string.toLowerCase() === compare.toLowerCase();
+	return string.toLowerCase() === compare.toLowerCase();
 }
 
 export function firstAlphabeticalOrder(first: string, second: string, caseSensitive: boolean = true) {
-    if (caseSensitive) {
-        return first < second;
-    }
-    return first.toLowerCase() < second.toLowerCase();
+	if (caseSensitive) {
+		return first < second;
+	}
+	return first.toLowerCase() < second.toLowerCase();
 }
 
 export function firstAlphabeticalOrderInsensitive(first: string, second: string) {
-    return firstAlphabeticalOrder(first, second, false);
+	return firstAlphabeticalOrder(first, second, false);
 }
 
 export function firstAlphabeticalOrderSensitive(first: string, second: string) {
-    return firstAlphabeticalOrder(first, second);
+	return firstAlphabeticalOrder(first, second);
 }
 
 export const strings = {
-    equalsCaseInsensitive,
-    firstAlphabeticalOrder,
-    firstAlphabeticalOrderInsensitive,
-    firstAlphabeticalOrderSensitive,
-    strEquals,
+	equalsCaseInsensitive,
+	firstAlphabeticalOrder,
+	firstAlphabeticalOrderInsensitive,
+	firstAlphabeticalOrderSensitive,
+	strEquals,
 };

65b330522631a8207b61e0d74b71a3eff26bc8a8
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:35:48 -0200

    Prettier pipe

--- src/functional/Pipe.ts
+++ src/functional/Pipe.ts
@@ -1,7 +1,7 @@
 export function composeRight(...fns: Function[]) {
-    return fns.reduce((f, g) => (...args: any) => g(f(...args)));
+	return fns.reduce((f, g) => (...args: any) => g(f(...args)));
 }
 
 export function composeLeft(...fns: Function[]) {
-    return fns.reduce((f, g) => (...args: any) => f(g(...args)));
+	return fns.reduce((f, g) => (...args: any) => f(g(...args)));
 }

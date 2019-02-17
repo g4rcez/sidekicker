0ae087ddbf40bb54aa6f069a77a5168707e64d8d
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:41:28 -0200

    Prettier format

--- src/strings/Format.ts
+++ src/strings/Format.ts
@@ -2,45 +2,46 @@ import { REGEX_CEP } from "../regex/BrazilianRegex";
 import { onlyNumbers } from "./Utils";
 
 export function formatCep(text: string) {
-    return text.trim().replace(REGEX_CEP, "$1$2-$3");
+	return text.trim().replace(REGEX_CEP, "$1$2-$3");
 }
 
 export function formatCpf(string: string) {
-    return onlyNumbers(string)
-        .replace(/(\d{3})(\d)/, "$1.$2")
-        .replace(/(\d{3})(\d)/, "$1.$2")
-        .replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+	return onlyNumbers(string).replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+}
+
+export function formatCnpj(string: string) {
+	return onlyNumbers(string).replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
 }
 
 export function formatDecimals(target: string | number, places: number) {
-    return parseFloat(target.toString()).toFixed(places);
+	return parseFloat(target.toString()).toFixed(places);
 }
 
 export function formatCardNumber(value: string) {
-    return onlyNumbers(value)
-        .replace(/(\d{4})/g, "$1 ")
-        .trim();
+	return onlyNumbers(value)
+		.replace(/(\d{4})/g, "$1 ")
+		.trim();
 }
 
 export function formatPhoneDDD(phone: string) {
-    return onlyNumbers(phone).replace(/(\d\d)(9\d{4})(\d{4})/, "($1) $2-$3");
+	return onlyNumbers(phone).replace(/(\d\d)(9\d{4})(\d{4})/, "($1) $2-$3");
 }
 
 export function formatPhone(phone: string) {
-    const string = onlyNumbers(phone);
-    if (string.length === 8) {
-        return string.replace(/(\d{4})(\d{4})/, "$1-$2");
-    } else if (string.length === 9) {
-        return string.replace(/(9\d{4})(\d{4})/, "$1-$2");
-    }
-    return formatPhoneDDD(string);
+	const string = onlyNumbers(phone);
+	if (string.length === 8) {
+		return string.replace(/(\d{4})(\d{4})/, "$1-$2");
+	} else if (string.length === 9) {
+		return string.replace(/(9\d{4})(\d{4})/, "$1-$2");
+	}
+	return formatPhoneDDD(string);
 }
 
 export const format = {
-    formatCardNumber,
-    formatCep,
-    formatCpf,
-    formatDecimals,
-    formatPhone,
-    formatPhoneDDD,
+	formatCardNumber,
+	formatCep,
+	formatCpf,
+	formatDecimals,
+	formatPhone,
+	formatPhoneDDD,
 };

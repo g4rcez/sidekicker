79d841c2e1f6c2b340da5321996f2c38012f7d88
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:30:26 -0200

    Create function to validate CNPJ

--- src/validations/CNPJ.ts
+++ src/validations/CNPJ.ts
@@ -1,26 +1,37 @@
 import { onlyNumbers } from "../strings/Utils";
 
-const reduceFunction = (index: number) => (buffer: number, number: number) => {
-    buffer += number * index;
-    index = index === 9 ? 2 : index + 1;
-    return buffer;
-};
-
-const verifyingDigit = (numbers: string) => {
-    const index = 2;
-    const reverse = numbers.split("").reduce((buffer, number) => [+number].concat(buffer), []);
-    const sum = reverse.reduce(reduceFunction(index), 0);
-    const mod = sum % 11;
-    return mod < 2 ? 0 : 11 - mod;
-};
+const getChar = (str: string, position: number) => Number.parseInt(str.charAt(position));
 
 export function isCnpj(cnpj: string) {
-    const cleaned = onlyNumbers(cnpj);
-    if (!cleaned || cleaned.length !== 14 || /^(\d)\1+$/.test(cleaned)) {
-        return false;
-    }
-    let registration = cleaned.substr(0, 12);
-    registration += verifyingDigit(registration);
-    registration += verifyingDigit(registration);
-    return registration.substr(-2) === cleaned.substr(-2);
+	const val = onlyNumbers(cnpj);
+	if (/0{14}/.test(val) || val.length != 14) {
+		return false;
+	}
+	let length = val.length - 2;
+	const digits = val.substring(length);
+	let numbers = val.substring(0, length);
+	let soma = 0;
+	let position = length - 7;
+	for (let i = length; i >= 1; i--) {
+		soma += getChar(numbers, length - i) * position--;
+		if (position < 2) {
+			position = 9;
+		}
+	}
+	let resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+	if (resultado != getChar(digits, 0)) {
+		return false;
+	}
+	length = length + 1;
+	numbers = val.substring(0, length);
+	soma = 0;
+	position = length - 7;
+	for (let i = length; i >= 1; i--) {
+		soma += getChar(numbers, length - i) * position--;
+		if (position < 2) {
+			position = 9;
+		}
+	}
+	resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+	return resultado == getChar(digits, 1);
 }

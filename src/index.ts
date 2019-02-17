fc0f0794d3c8431f72e606421f9da7a3e7226821
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:38:54 -0200

    Arrange exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export { default as Validations } from "./validations";
-export { default as Collection } from "./iterable";
-export { default as Strings } from "./strings";
+export { default as Validations, Validator } from "./validations";
+export { default as Collection, Linq } from "./iterable";
+export { default as Strings, format as Format, utils as Utils } from "./strings";
 export { default as Regex } from "./regex";
-export { default as Linq } from './iterable/linq/index'
 export { default as Functional } from "./functional";

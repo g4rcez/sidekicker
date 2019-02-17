64464922d399ba3917e4dd320bdb584b37b24bb6
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:36:48 -0200

    Prettier clipboard

--- src/html/CopyClipboard.ts
+++ src/html/CopyClipboard.ts
@@ -1,6 +1,6 @@
 export function copyToClipboard(id: string) {
-    const text: any = document.getElementById(id);
-    text.select();
-    document.execCommand("copy");
-    text.blur();
+	const text: any = document.getElementById(id);
+	text.select();
+	document.execCommand("copy");
+	text.blur();
 }

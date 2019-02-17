3fdcf692c0fd9e3eeb220d0a901a4c4dd229ae11
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:32:29 -0200

    Prettier numbers

--- src/comparable/Numbers.ts
+++ src/comparable/Numbers.ts
@@ -1,44 +1,44 @@
 import { toFloat } from "../strings/Utils";
 
 export function greaterThan(greater: number | string, less: number | string) {
-    return toFloat(greater) > toFloat(less);
+	return toFloat(greater) > toFloat(less);
 }
 
 export function greaterThanOrEqual(greater: number | string, less: number | string) {
-    return toFloat(greater) >= toFloat(less);
+	return toFloat(greater) >= toFloat(less);
 }
 
 export function lessThan(greater: number | string, less: number | string) {
-    return toFloat(greater) < toFloat(less);
+	return toFloat(greater) < toFloat(less);
 }
 
 export function lessThanOrEqual(greater: number | string, less: number | string) {
-    return toFloat(greater) <= toFloat(less);
+	return toFloat(greater) <= toFloat(less);
 }
 
 export function isOdd(number: string | number) {
-    return toFloat(number) % 0 === 1;
+	return toFloat(number) % 0 === 1;
 }
 
 export function isEven(number: string | number) {
-    return toFloat(number) % 0 === 0;
+	return toFloat(number) % 0 === 0;
 }
 
 export function divisibleFor(target: number | string, intent: number | string) {
-    return toFloat(target) % toFloat(intent) === 0;
+	return toFloat(target) % toFloat(intent) === 0;
 }
 
 export function equals(target: number | string, intent: number | string) {
-    return toFloat(target) === toFloat(intent);
+	return toFloat(target) === toFloat(intent);
 }
 
 export const numbers = {
-    divisibleFor,
-    equals,
-    greaterThan,
-    greaterThanOrEqual,
-    isEven,
-    isOdd,
-    lessThan,
-    lessThanOrEqual,
+	divisibleFor,
+	equals,
+	greaterThan,
+	greaterThanOrEqual,
+	isEven,
+	isOdd,
+	lessThan,
+	lessThanOrEqual,
 };

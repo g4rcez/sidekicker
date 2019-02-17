d32c6757c1c7840374597f8a822bdf31f31be259
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:41:43 -0200

    Prettier unix functions

--- src/strings/unix/Cut.ts
+++ src/strings/unix/Cut.ts
@@ -1,46 +1,46 @@
 import { toInt } from "../Utils";
 
 interface ICut {
-    text: string;
-    delimiter?: string;
-    fields?: string;
-    chars?: number;
-    string?: boolean;
+	text: string;
+	delimiter?: string;
+	fields?: string;
+	chars?: number;
+	string?: boolean;
 }
 
 const rangeRegex = /^[0-9]+-$/;
 const multipleRegex = /^\d+(|,\d+)+$/;
 
 const likeString = (array: string[], like: boolean, delimiter: string) => {
-    if (like) {
-        return array.join(delimiter);
-    }
-    return array;
+	if (like) {
+		return array.join(delimiter);
+	}
+	return array;
 };
 
 export const cut = (cmd: ICut) => {
-    if (cmd.delimiter && !cmd.fields) {
-        return likeString(cmd.text.split(cmd.delimiter), cmd.string, cmd.delimiter);
-    }
-    if (cmd.delimiter && cmd.fields) {
-        const textSplit = cmd.text.split(cmd.delimiter);
-        const fieldArgs = cmd.fields.split("-");
-        const total = textSplit.length;
-        const first = toInt(fieldArgs[0]);
-        if (rangeRegex.test(cmd.fields)) {
-            return likeString(textSplit.splice(first - 1, total), cmd.string, cmd.delimiter);
-        }
-        if (multipleRegex.test(cmd.fields)) {
-            const array: string[] = [];
-            cmd.fields.split(",").forEach((x) => {
-                const fieldNumber = toInt(x) - 1;
-                try {
-                    array.push(textSplit[fieldNumber]);
-                } catch (e) {}
-            });
-            return likeString(array, cmd.string, cmd.delimiter);
-        }
-        const second = toInt(fieldArgs[1]) || first;
-        return likeString(textSplit.splice(first - 1, second), cmd.string, cmd.delimiter);
-    }
+	if (cmd.delimiter && !cmd.fields) {
+		return likeString(cmd.text.split(cmd.delimiter), cmd.string, cmd.delimiter);
+	}
+	if (cmd.delimiter && cmd.fields) {
+		const textSplit = cmd.text.split(cmd.delimiter);
+		const fieldArgs = cmd.fields.split("-");
+		const total = textSplit.length;
+		const first = toInt(fieldArgs[0]);
+		if (rangeRegex.test(cmd.fields)) {
+			return likeString(textSplit.splice(first - 1, total), cmd.string, cmd.delimiter);
+		}
+		if (multipleRegex.test(cmd.fields)) {
+			const array: string[] = [];
+			cmd.fields.split(",").forEach((x) => {
+				const fieldNumber = toInt(x) - 1;
+				try {
+					array.push(textSplit[fieldNumber]);
+				} catch (e) {}
+			});
+			return likeString(array, cmd.string, cmd.delimiter);
+		}
+		const second = toInt(fieldArgs[1]) || first;
+		return likeString(textSplit.splice(first - 1, second), cmd.string, cmd.delimiter);
+	}
 };
--- src/strings/unix/Tr.ts
+++ src/strings/unix/Tr.ts
@@ -1,20 +1,20 @@
 interface ITR {
-    text: string;
-    subs1: string;
-    subs2: string;
+	text: string;
+	subs1: string;
+	subs2: string;
 }
 
 export const tr = (parameters: ITR) => {
-    const text = [...parameters.text];
-    if (parameters.subs1.length === parameters.subs2.length) {
-        [...parameters.subs1].forEach((x, i) => {
-            if (text.indexOf(x) !== parameters.text.search(x)) {
-                text[text.indexOf(x)] = parameters.subs2[i];
-            } else {
-                text[parameters.text.search(x)] = parameters.subs2[i];
-            }
-        });
-        return text.join("");
-    }
-    return parameters.text;
+	const text = [...parameters.text];
+	if (parameters.subs1.length === parameters.subs2.length) {
+		[...parameters.subs1].forEach((x, i) => {
+			if (text.indexOf(x) !== parameters.text.search(x)) {
+				text[text.indexOf(x)] = parameters.subs2[i];
+			} else {
+				text[parameters.text.search(x)] = parameters.subs2[i];
+			}
+		});
+		return text.join("");
+	}
+	return parameters.text;
 };

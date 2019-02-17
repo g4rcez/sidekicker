8b133bf480cb3fcd1636b8e16306276d12dfa019
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:35:04 -0200

    prettier date conversions

--- src/dates/conversions.ts
+++ src/dates/conversions.ts
@@ -1,13 +1,13 @@
 import moment from "moment";
 
 export function epochToDate(epoch: number, format = "YYYY-MM-dd") {
-    return moment.unix(epoch).format(format);
+	return moment.unix(epoch).format(format);
 }
 
 export function dateFormat(date: string, format = "DD MMM") {
-    return moment(date).format(format);
+	return moment(date).format(format);
 }
 
 export function timeFormat(date: string, format = "HH:MM") {
-    return moment(date).format(format);
+	return moment(date).format(format);
 }

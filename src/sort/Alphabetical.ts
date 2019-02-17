7af523e0ee33e280674f68187edd2eea110daa68
Garcez <allan.f.garcez@gmail.com>
2019-02-16 22:39:35 -0200

    Prettier sorts

--- src/sort/Alphabetical.ts
+++ src/sort/Alphabetical.ts
@@ -1,17 +1,17 @@
 import * as Strings from "../comparable/Strings";
 
 const insensitive = (a: string, b: string) => {
-    if (Strings.strEquals(a, b)) {
-        return 0;
-    }
-    return Strings.firstAlphabeticalOrderInsensitive(a, b) ? -1 : 1;
+	if (Strings.strEquals(a, b)) {
+		return 0;
+	}
+	return Strings.firstAlphabeticalOrderInsensitive(a, b) ? -1 : 1;
 };
 
 const sensitive = (a: string, b: string) => {
-    if (Strings.strEquals(a, b)) {
-        return 0;
-    }
-    return Strings.firstAlphabeticalOrderSensitive(a, b) ? -1 : 1;
+	if (Strings.strEquals(a, b)) {
+		return 0;
+	}
+	return Strings.firstAlphabeticalOrderSensitive(a, b) ? -1 : 1;
 };
 
 export default { sensitive, insensitive };

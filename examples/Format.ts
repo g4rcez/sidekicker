032f5e10085284ddd41cd0aa24c5a51f3bf902c0
Garcez <allan.f.garcez@gmail.com>
2019-02-27 00:59:41 -0300

    version 0.0.9

---
+++ examples/Format.ts
@@ -0,0 +1,8 @@
+import { Format } from "../lib";
+
+console.log(Format.formatCpf("11663283761"));
+console.log(Format.formatCnpj("11111111111111"));
+console.log(Format.formatCep("36021070"));
+console.log(Format.formatPhone("32984047599"));
+console.log(Format.formatPhone("984047599"));
+console.log(Format.formatPhoneCountryCode("5532984047599"));
--- examples/Linq.ts
+++ examples/Linq.ts
@@ -1,22 +1,18 @@
 import { Linq } from "../lib";
 
-const list = Linq([
-    { id: 1, value: 50 },
-    { id: 2, value: 50 },
-    { id: 3, value: 50 },
-    { id: 4, value: 50 },
-    { id: 5, value: 50 },
-    { id: 6, value: 50 },
-    { id: 7, value: 50 },
-    { id: 8, value: 50 },
+const list = Linq<{ id: number; value: number }>([
+	{ id: 1, value: 10 },
+	{ id: 2, value: 20 },
+	{ id: 3, value: 30 },
+	{ id: 4, value: 40 },
+	{ id: 5, value: 50 },
+	{ id: 6, value: 50 },
+	{ id: 7, value: 60 },
+	{ id: 8, value: 70 },
 ])
-    .where({
-        key: "value",
-        operator: ">",
-        value: 10,
-    })
-    .uniqBy("id")
-    .reverse()
-    .orderBy("value");
+	.where("value", "===", 50)
+	.uniqBy("id")
+	.reverse()
+	.orderBy("value");
 console.log(list.select());
 console.log(list.sum("value"));

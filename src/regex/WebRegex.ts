7d552f1a8b4290c376d912960f48f0366ed511e5
Garcez <allan.f.garcez@gmail.com>
2019-02-27 00:57:51 -0300

    URL regex

--- src/regex/WebRegex.ts
+++ src/regex/WebRegex.ts
@@ -1,8 +1,6 @@
 // tslint:disable-next-line:max-line-length
 export const email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export const http = /^(https?:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
-
 export const username = /^[a-z][a-z0-9_-]{3,32}$/;
 
 export const hexColor = /^#([a-f0-9]{3}|[a-f0-9]{6})$/;

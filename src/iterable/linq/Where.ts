891f9a7bb88dcdbbd5fa1686c69e4c1b58800bfb
Garcez <allan.f.garcez@gmail.com>
2019-02-27 00:58:35 -0300

    Change where parameters in where file

--- src/iterable/linq/Where.ts
+++ src/iterable/linq/Where.ts
@@ -1,12 +1,13 @@
 import { WhereOperator } from "WhereOperator";
 import { executeByOperator } from "./Operator";
 
-const where = (values: WhereOperator) => {
+const where = (values: { key: string; operator: WhereOperator; value: any }) => {
 	return (array: any[]) => {
+		const key = values.key;
 		if (typeof values.value === "function") {
-			return array.filter((x) => values.value(x[values.key]));
+			return array.filter((x) => values.value(x[key]));
 		}
-		return array.filter((element) => executeByOperator(element[values.key], values.operator, values.value));
+		return array.filter((element) => executeByOperator(element[key], values.operator, values.value));
 	};
 };
 export default where;

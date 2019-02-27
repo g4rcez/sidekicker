1a6b5d13d03056940c92fb5eb028f3a09741c182
Garcez <allan.f.garcez@gmail.com>
2019-02-27 00:58:22 -0300

    Add operators

--- src/iterable/linq/Operator.ts
+++ src/iterable/linq/Operator.ts
@@ -3,25 +3,25 @@ import { isAfter, isBefore, isSameOrAfter, isSameOrBefore } from "../../dates/Da
 
 export const operators: Operator = {
 	// tslint:disable-next-line:triple-equals
-	"!=": (x: any, y: any) => x != y,
-	"!==": (x: any, y: any) => x !== y,
+	"!=": (x: unknown, y: unknown) => x != y,
+	"!==": (x: unknown, y: unknown) => x !== y,
 	"<": (x: any, y: any) => x < y,
 	"<=": (x: any, y: any) => x <= y,
 	// tslint:disable-next-line:triple-equals
-	"==": (x: any, y: any) => x == y,
-	"===": (x: any, y: any) => x === y,
+	"==": (x: unknown, y: unknown) => x == y,
+	"===": (x: unknown, y: unknown) => x === y,
+	equals: (x: unknown, y: unknown) => x === y,
 	">": (x: any, y: any) => x > y,
 	">=": (x: any, y: any) => x >= y,
 	isAfter,
 	isBefore,
 	isSameOrAfter,
 	isSameOrBefore,
-	like: (x: any, y: any) => {
-		const str: string = x.toString();
-		return !!str.match(new RegExp(`.*${y.toString()}.*`, "gi"));
+	like: (x: string, y: string) => {
+		return !!x.match(new RegExp(`.*${y}.*`, "gi"));
 	},
 };
 
-export const executeByOperator = (x: any, operator: string, y: any) => operators[operator](x, y);
+export const executeByOperator = (x: unknown, operator: string, y: unknown) => operators[operator](x, y);
 
 export default { executeByOperator, operators };

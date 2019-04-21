d458f8d69fd859642f307bce1e4186d78726dbf9
Garcez <allan.f.garcez@gmail.com>
2019-04-21 00:56:21 -0300

    /home/garcez/.asdf/installs/nodejs/11.14.0/bin/node

--- magik.js
+++ magik.js
@@ -2,41 +2,60 @@ const fs = require("fs");
 const argv = process.argv.splice(0, 1);
 const { exec } = require("child_process");
 
-const shell = (command) => {
-	return new Promise((res, rej) => {
+const shell = (command) =>
+	new Promise((res, rej) =>
 		exec(command, (err, stdout, stderr) => {
 			if (err) {
 				return rej([false, stderr]);
 			}
 			return res([true, stdout]);
-		});
-	});
-};
+		}),
+	);
 
 const read = (file) =>
 	new Promise((res, rej) => fs.readFile(file, "utf8", (err, content) => (err ? rej(err) : res(content))));
 
-const write = (file, content) => {
-	return new Promise((res, rej) => {
-		return fs.writeFile(file, JSON.stringify(content, null, 4), (err) => {
+const write = (file, content) =>
+	new Promise((res, rej) =>
+		fs.writeFile(file, JSON.stringify(content, null, 4), (err) => {
 			return err ? rej([false, err]) : res(true);
-		});
-	});
-};
+		}),
+	);
 
 const message = argv[0];
 const versionInc = argv[1] || "beta";
 
 const calculateVersion = (increment, versions = ["0", "0", "0"]) => {
-	const release = Number.parseInt(versions[0], 10) + 1;
-	const pre = Number.parseInt(versions[1], 10) + 1;
 	const last = Number.parseInt(versions[2], 10) + 1;
 	if (increment === "beta") {
 		return `${versions[0]}.${versions[1]}.${last}`;
-	} else if (increment === "preRelease") {
+	}
+	const pre = Number.parseInt(versions[1], 10) + 1;
+	if (increment === "preRelease") {
 		return `${versions[0]}.${pre}.${last}`;
 	}
+	const release = Number.parseInt(versions[0], 10) + 1;
 	return `${release}.${pre}.${last}`;
+};
+
+const gitAdd = async () => {
+	const cmd = await shell("git add .");
+	console.log("Adicionando arquivos", cmd[1]);
+};
+
+const gitCommit = async (version) => {
+	await shell(`git commit -m "${message || version}"`);
+	console.log("Increment to version: ", version);
+};
+
+const gitTag = async (version) => {
+	const tag = await shell(`git tag v${version}`);
+	console.log(`Create tag: v${version}`, tag[1]);
+};
+
+const gitPush = async (version) => {
+	const push = await shell(`git push origin v${version}`);
+	console.log("Push tag", push[1]);
 };
 
 read("./package.json")
@@ -46,18 +65,14 @@ read("./package.json")
 		write("./package.json", { ...content, version })
 			.then(async (e) => {
 				try {
-					const cmd = await shell("git add .");
-					console.log("Adicionando arquivos", cmd[1]);
-					await shell(`git commit -m "${message || version}"`);
-					console.log("Increment to version: ", version);
-					const tag = await shell(`git tag v${version}`);
-					console.log(`Create tag: v${version}`, tag[1]);
-					const push = await shell(`git push origin v${version}`);
-					console.log(push);
-				} catch (error) {
-					console.log(error);
+					await gitAdd();
+					await gitCommit(version);
+					await gitTag(version);
+					await gitPush(version);
+				} catch (e) {
+					console.log(e);
 				}
 			})
-			.catch((e) => console.log("Deu ruim no write"));
+			.catch((e) => console.log(e));
 	})
-	.catch((e) => console.log("Deu ruim", e));
+	.catch((e) => console.log(e));

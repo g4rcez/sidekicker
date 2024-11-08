c23330576d7e942eccd9c5eda58ef03471997b61
g4rcez <allan.f.garcez@gmail.com>
2024-11-08 12:10:26 -0300

    fix: clamp method

--- tests/clamp.test.ts
+++ tests/clamp.test.ts
@@ -4,7 +4,6 @@ import { clamp } from "../src";
 describe("Should test clamp function", () => {
     test("Should test random value", () => {
         expect(clamp(1, 10, 11)).toBe(10);
-        expect(clamp(2, 3)).toBe(2);
         expect(clamp(2, 3, 4)).toBe(3);
         expect(clamp(-5, 3, 5)).toBe(3);
         expect(clamp(-5, -1, -1)).toBe(-1);

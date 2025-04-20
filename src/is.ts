0c05ddb274646e13fa4d085209314cbc18083730
g4rcez <allan.f.garcez@gmail.com>
2025-04-19 22:51:13 -0300

    fix: Is.number validation

--- src/is.ts
+++ src/is.ts
@@ -18,15 +18,32 @@ export const isInstance = <C extends Instance>(a: any, clazz: C): a is C => a in
 
 export const isPrimitive = <A>(a: A): boolean => {
     const type = typeof a;
-    return type === "undefined" || type === null || type === "number" || type === "string" || type === "boolean" || type === "bigint";
+    return (
+        type === "undefined" ||
+        type === null ||
+        type === "number" ||
+        type === "string" ||
+        type === "boolean" ||
+        type === "bigint"
+    );
 };
 
-export const isDate = (a: any): a is Date => Object.prototype.toString.call(a) === "[object Date]" && isInstance(a, Date);
+export const isDate = (a: any): a is Date =>
+    Object.prototype.toString.call(a) === "[object Date]" && isInstance(a, Date);
 
 export const Is = {
     array,
     date: isDate,
-    empty: (a: any) => isNull(a) || isUndefined(a) ? true : typeof a === "object" ? Object.keys(a).length === 0 : typeof a === "string" ? a === "" : Array.isArray(a) ? a.length === 0 : Number.isNaN(a),
+    empty: (a: any) =>
+        isNull(a) || isUndefined(a)
+            ? true
+            : typeof a === "object"
+              ? Object.keys(a).length === 0
+              : typeof a === "string"
+                ? a === ""
+                : Array.isArray(a)
+                  ? a.length === 0
+                  : Number.isNaN(a),
     function: (a: any): a is Fn => typeof a === "function",
     instance: isInstance,
     isError,
@@ -34,9 +51,14 @@ export const Is = {
     nan: (a: any): a is NaN => Number.isNaN(a),
     nil: (a: any): a is undefined | null => isNull(a) || isUndefined(a),
     null: isNull,
-    number: (a: any): a is number => typeof a === "number" || !Number.isNaN(a),
+    number: (a: any): a is number => {
+        if (typeof a === "number") {
+            return !Number.isNaN(a);
+        }
+        return false;
+    },
     object: <T = object>(a: any): a is T => !array(a) && typeof a === "object",
     primitive: isPrimitive,
     string: (a: any): a is string => typeof a === "string",
-    undefined: isUndefined
+    undefined: isUndefined,
 };
